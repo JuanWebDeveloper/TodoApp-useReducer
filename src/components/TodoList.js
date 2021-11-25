c6c5b46e15b2c93c42acb2036fd6a8269d9718b2
Juan Vargas <juandeveloper19@gmail.com>
2021-11-25 02:38:54 -0500

    Defining the styles of the TodoList component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
+import '../styles/todoList.css';
+
 export const TodoList = () => {
 	return (
 		<div className='todo-list_container'>
 			<h2>Todo List</h2>
-			<hr />
 
 			<ul>
 				<li>
-					<p>1. Example</p>
+					<p>
+						<span>1.</span> Example
+					</p>
 					<div className='action-buttons'>
+						<h5>Actions</h5>
 						<button className='edit'>
 							<i class='fas fa-edit'></i>
 						</button>
