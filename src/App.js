8aa167450bb47682dd3848eec602908edfbec7b8
Juan Vargas <juandeveloper19@gmail.com>
2021-11-25 01:55:33 -0500

    Defining the default styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { TodoAdd } from './components/TodoAdd';
 import { TodoList } from './components/TodoList';
 
+import './styles/default.css';
+
 const App = () => {
 	return (
 		<div className='main-container'>
