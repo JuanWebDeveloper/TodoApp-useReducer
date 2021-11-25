c9f2447b039700de3c3cdd1b445a9afae5dcb6fa
Juan Vargas <juandeveloper19@gmail.com>
2021-11-25 01:58:13 -0500

    Defining the styles of the TodoAdd component

--- src/components/TodoAdd.js
+++ src/components/TodoAdd.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
+import '../styles/todoAdd.css';
+
 export const TodoAdd = () => {
 	return (
 		<div className='form-container'>
 			<h2>Add Todo</h2>
-			<hr />
 
 			<form>
-				<input type='text' name='description' autoComplete='off' />
+				<input type='text' name='description' placeholder='Add Description' autoComplete='off' />
 				<button type='submit'>Add</button>
 			</form>
 		</div>
